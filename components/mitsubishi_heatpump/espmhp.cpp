1c5bd2a94ed44731a78db2928097d55a51c39fc5
vge <victorgolfecho@gmail.com>
2024-12-23 16:19:38 -0800

    Remove update logging

--- components/mitsubishi_heatpump/espmhp.cpp
+++ components/mitsubishi_heatpump/espmhp.cpp
@@ -76,7 +76,6 @@ void MitsubishiHeatPump::banner() {
 }
 
 void MitsubishiHeatPump::update() {
-    ESP_LOGI(TAG, "MitsubishiHeatPump::update");
     // This will be called every "update_interval" milliseconds.
     //this->dump_config();
     this->hp->sync();
